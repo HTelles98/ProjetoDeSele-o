9c19d1cc91ba0329c14511ba8b2245140072dcc6
Henrique Telles <116848118+HTelles98@users.noreply.github.com>
2024-03-05 15:02:43 -0300

    Add files via upload

---
+++ src/componentes/banner/banner.jsx
@@ -0,0 +1,12 @@
+import "./banner.css"
+
+export const Banner = () => {
+     return (
+        <header className="banner">
+          <img src="/imagens/bannerFort2.jpg" alt="Banner principal da pagina" />
+        </header>
+        
+     )
+}
+
+export default Banner
